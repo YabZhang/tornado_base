8dfdd329b57f67caf56e3ddf8ec9b5a09ff5b95d
Yabin Zhang <ybzhang2015@gmail.com>
2017-05-14 03:31:34 +0800

    add jwt login handler

--- app/base.py
+++ app/base.py
@@ -6,6 +6,7 @@ import tornado.ioloop
 
 from tornado.web import Application
 from tornado.web import RequestHandler
+from tornado.web import authenticated
 from tornado.options import define, options, parse_command_line
 
 define("host", default="127.0.0.1", type=str)
@@ -24,10 +25,9 @@ class JWTERROR(Exception):
     pass
 
 class JWTHandler(RequestHandler):
-    """ handle JSON WEB TOKEN """
-
-    def prepare(self):
-        pass
+    """ handle JSON WEB TOKEN
+        解析作为认证token使用
+    """
 
     def __jwt_encode(self, payload):
         jwt_secret = self.application.settings.get("jwt_secret")
@@ -57,11 +57,34 @@ class BaseHandler(JWTHandler):
     """ request handler baseclass """
 
     def get_current_user(self):
-        pass
+        auth = self.__jwt_decode()
+        if auth.get("user_id"):
+            return auth["user_id"]
+
 
 class MainHandler(BaseHandler):
     """ main request handler """
-    pass
+    @authenticated
+    def get(self):
+        pass
+
+
+class LoginHandler(BaseHandler):
+    """ login request handler"""
+    # TODO: json response
+    def post(self):
+        username = self.get_argument("username")
+        passwd = self.get_argument("passwd")
+        user_id = self.__check_credential(username, passwd)
+        if user_id:    # user_id
+            token = self.__jwt_encode({"user_id": result})
+            return self.write({'msg': 'success', 'token': token})
+        self.set_status(400)
+        return self.write({'msg': 'failure'})
+
+    def __check_credential(self, username, passwd):
+        """ calc md5 and compare with value in db """
+        pass
 
 
 def main():
@@ -71,13 +94,15 @@ def main():
         "debug": True,
         "xsrf_secret": True,
         "cookie_secret": "COOKIE_SECRET",
-        "jwt_serret": "JWT_SECRET"
+        "jwt_serret": "JWT_SECRET",
+        "login_url": "/login"
     }
 
     app = Application(
         # urls
         [
             ("/", MainHandler),
+            ("/login", LoginHandler)
         ],
         # settings
         **settings
